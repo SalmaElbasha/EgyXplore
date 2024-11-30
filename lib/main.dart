47a6f2fe01be8a6cf16a564987df71c4d48e6b85
mazenelgaml <112718739+mazenelgaml@users.noreply.github.com>
2024-11-30 02:41:53 +0200

    ResetPasswordScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:egyxplore/screens/forget_password/forget_password_screen.dart';
 import 'package:egyxplore/screens/login_screen/login_screen.dart';
 import 'package:egyxplore/screens/please_check_your_email/please_check_your_email_screen.dart';
+import 'package:egyxplore/screens/reset_password/reset_password_screen.dart';
 
 import 'package:egyxplore/screens/signup_screen/signup_screen.dart';
 
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       title: 'EgyXplore',
       debugShowCheckedModeBanner: false,
 
-      home: PleaseCheckYourEmailScreen(),
+      home: ResetPasswordScreen(),
 
     );
   }
--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -3,20 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key,  required this.text, this.suffixIcon});
+  const CustomTextFormField({
+    super.key,
+    required this.text,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.onSuffixIconTap,
+  });
+
   final String text;
   final Widget? suffixIcon;
+  final bool obscureText;
+  final VoidCallback? onSuffixIconTap;
+
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      width: Get.width * 0.7, // Example: 70% of the screen width
+    return SizedBox(
+      width: Get.width * 0.7,
       height: 61,
       child: TextFormField(
+        obscureText: obscureText,
         decoration: InputDecoration(
-          suffixIcon: suffixIcon??null,
+          suffixIcon: InkWell(
+            onTap: onSuffixIconTap,
+            child: suffixIcon,
+          ),
           labelText: text,
           labelStyle: const TextStyle(
-
             color: Colors.brown,
             fontSize: 16,
           ),
@@ -24,27 +37,19 @@ class CustomTextFormField extends StatelessWidget {
           contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30),
-            borderSide: BorderSide(
-                color: Colors.brown,
-                width: 2
-            ),
+            borderSide: BorderSide(color: Colors.brown, width: 2),
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30),
-            borderSide: const BorderSide(
-              color: Colors.brown,
-              width: 2.0,
-            ),
+            borderSide: const BorderSide(color: Colors.brown, width: 2.0),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30),
-            borderSide: const BorderSide(
-              color: Colors.brown,
-              width: 2.0,
-            ),
+            borderSide: const BorderSide(color: Colors.brown, width: 2.0),
           ),
         ),
       ),
     );
   }
 }
+
