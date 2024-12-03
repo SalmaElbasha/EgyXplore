9af44f1e99f9612ec05023e8c5b61b47a64a2800
mazenelgaml <112718739+mazenelgaml@users.noreply.github.com>
2024-12-03 20:13:12 +0200

    QuestionScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:egyxplore/screens/Question/Question_Screen.dart';
 import 'package:egyxplore/screens/forget_password/forget_password_screen.dart';
 import 'package:egyxplore/screens/login_screen/login_screen.dart';
 import 'package:egyxplore/screens/please_check_your_email/please_check_your_email_screen.dart';
@@ -8,6 +9,7 @@ import 'package:egyxplore/screens/signup_screen/signup_screen.dart';
 
 import 'package:egyxplore/screens/splash/splash_Screen.dart';
 import 'package:egyxplore/screens/welcome/welcome_screen.dart';
+import 'package:egyxplore/widgets/custom_back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
       title: 'EgyXplore',
       debugShowCheckedModeBanner: false,
 
-      home: ResetPasswordScreen(),
+      home: QuestionScreen(),
 
     );
   }
